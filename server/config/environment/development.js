0b0cca00eaa0850428e59a8dfb413d8f67004a35
Tomás Dias Almeida <tomas.almeida@gmail.com>
2015-03-29 23:43:16 +0200

    Initial Commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/minhatribo-dev'
+  },
+
+  seedDB: true
+};
